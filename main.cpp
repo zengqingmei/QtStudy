c9f484f12b6d0f05a366bb807b09e84bd2e19bb1
zengqingmei <342290181@qq.com>
2022-12-12 08:55:50 +0800

    Update

--- main.cpp
+++ main.cpp
@@ -6,9 +6,9 @@ int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
 	QTranslator *translator = new QTranslator();
-	translator->load("Resources/QtStudy_zh_Hans.qm");
+	translator->load("translations/QtStudy_zh_Hans.qm");
 	QTranslator *qtTranslator = new QTranslator();
-	qtTranslator->load("Resources/qt_zh_CN.qm");
+	qtTranslator->load("translations/qt_zh_CN.qm");
 	app.installTranslator(translator);
 	app.installTranslator(qtTranslator);
 	MainWindow *w = new MainWindow();
